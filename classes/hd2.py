21e3c1617403ef7d5211e694a37ac6eea31d711a
Matthew Bonney <71449379+msbonney@users.noreply.github.com>
2023-09-04 13:30:54 +0100

    Upgraded to include sparse matrices

--- classes/hd2.py
+++ classes/hd2.py
@@ -19,6 +19,21 @@ class hyperdual2:
                 self.d = np.zeros(sh)
             else:
                 self.d = d
+        elif sp.sparse.isspmatrix(a):
+            sh = a.shape
+            zr = sp.sparse.eye(sh[0],sh[1],format=a.format)-sp.sparse.eye(sh[0],sh[1],format=a.format)
+            if not sp.sparse.isspmatrix(b):
+                self.b = zr.copy()
+            else:
+                self.b = b
+            if not sp.sparse.isspmatrix(c):
+                self.c = zr.copy()
+            else:
+                self.c = c
+            if not sp.sparse.isspmatrix(d):
+                self.d = zr.copy()
+            else:
+                self.d = d
         else:
             self.b = b
             self.c = c
@@ -51,6 +66,8 @@ class hyperdual2:
         i2 = self.a*obj2.c + self.c*obj2.a
         i3 = self.a*obj2.d + self.b*obj2.c + self.c*obj2.b + self.d*obj2.a
         return hyperdual2(rl,i1,i2,i3)
+    def __rmul__(self,obj2): # Right multiple 2 hyperdual numbers
+        return hyperdual2.__mul__(self,obj2)
     def __truediv__(self,obj2): # Divide 2 hyperdual numbers
         if not isinstance(obj2,hyperdual2):
             obj2 = hyperdual2(obj2)
@@ -128,15 +145,81 @@ class hyperdual2:
         return np.sqrt(self.a**2)
     # Static methods
     @staticmethod
-    def real_eigs(K:np.matrix,M:np.matrix):
-        lamb,phi = sp.linalg.eig(K,M)
-        phi,lamb = np.matrix(phi), np.real(lamb) # Real values of eigenvalues only
+    def real_eigs(K,M,n:int=5):
+        # Format checking
+        if not isinstance(K,np.matrix):
+            if not sp.sparse.isspmatrix(K):
+                raise(Exception("Error in Stiffness data type"))
+            else:
+                flag_k = 1
+        else:
+            flag_k = 0
+        if not isinstance(M,np.matrix):
+            if not sp.sparse.isspmatrix(M):
+                raise(Exception("Error in Mass data type"))
+            else:
+                flag_m = 1
+        else:
+            flag_m = 0
+        if flag_m==1 & flag_k==1: # sparse
+            flag = 1
+        elif flag_m==1 & flag_k==0: #  sparse mass
+            form = M.format
+            if form == 'csc':
+                K = sp.sparse.csc_matrix(K)
+            elif form == 'bsr':
+                K = sp.sparse.bsr_matrix(K)
+            elif form == 'coo':
+                K = sp.sparse.coo_matrix(K)
+            elif form == 'csr':
+                K = sp.sparse.csr_matrix(K)
+            elif form == 'dia':
+                K = sp.sparse.dia_matrix(K)
+            elif form == 'dok':
+                K = sp.sparse.dok_matrix(K)
+            elif form == 'lil':
+                K = sp.sparse.lil_matrix(K)
+            else:
+                raise(Exception("Unknown sparse type"))
+            flag = 1
+        elif flag_m ==0 & flag_k ==1: # sparse stiffness
+            form = K.format
+            if form == 'csc':
+                M = sp.sparse.csc_matrix(M)
+            elif form == 'bsr':
+                M = sp.sparse.bsr_matrix(M)
+            elif form == 'coo':
+                M = sp.sparse.coo_matrix(M)
+            elif form == 'csr':
+                M = sp.sparse.csr_matrix(M)
+            elif form == 'dia':
+                M = sp.sparse.dia_matrix(M)
+            elif form == 'dok':
+                M = sp.sparse.dok_matrix(M)
+            elif form == 'lil':
+                M = sp.sparse.lil_matrix(M)
+            else:
+                raise(Exception("Unknown sparse type"))
+            flag = 1
+        elif flag_m==0 & flag_k==0: # dense
+            flag = 0
+        else:
+            raise(Exception("Sparse Flag error"))
+        # Split between sparse and dense
+        if flag: # Sparse
+            rnk = np.linalg.matrix_rank(K.todense())
+            n = np.min([n,rnk]) # Check is asking for more eigenvalues than physical
+            lamb,phi = sp.sparse.linalg.eigs(K,k=n,M=M,which='LM')
+            lamb,phi = np.real(lamb), np.matrix(phi)
+        else: # Dense
+            lamb,phi = sp.linalg.eig(K,M)
+            phi,lamb = np.matrix(phi), np.real(lamb) # Real values of eigenvalues only
         # Mass normalize mode shapes
         a = np.diag(phi.T*M*phi)
         phi_n = phi/np.sqrt(a)
         return lamb,phi_n
     @staticmethod
-    def eigs(K,M):
+    def eigs(K,M,n:int=5):
         if not isinstance(K,hyperdual2):
             x = hyperdual2(K)
         else:
@@ -146,14 +229,18 @@ class hyperdual2:
         else:
             y = M
         # Get real value eigenvalues/eigenvectors
-        lamb,phi = hyperdual2.real_eigs(x.a,y.a)
+        lamb,phi = hyperdual2.real_eigs(x.a,y.a,n)
+        phi = np.real(phi)
         nm,sha = len(lamb), phi.shape
         i1,i2,i3 = np.zeros(nm),np.zeros(nm),np.zeros(nm)
         phi_1,phi_2,phi_3 = np.matrix(np.zeros(sha)),np.matrix(np.zeros(sha)),np.matrix(np.zeros(sha))
         # Loop through modes
         for i in range(nm):
             # Some constants
-            Finv = np.linalg.inv(x.a-lamb[i]*y.a)
+            if sp.sparse.isspmatrix(x.a):
+                Finv = sp.sparse.linalg.inv(x.a - lamb[i]*y.a)
+            else:
+                Finv = np.linalg.inv(x.a-lamb[i]*y.a)
             # First Derivative Eigenvalue
             i1[i] = phi[:,i].T * (x.b-lamb[i]*y.b) * phi[:,i]
             i2[i] = phi[:,i].T * (x.c-lamb[i]*y.c) * phi[:,i]
---
+++ sp_example.py
@@ -0,0 +1,109 @@
+#%% Load in libraries
+import numpy as np  
+import scipy.sparse as sparse
+import scipy as sp
+from classes.hd2 import hyperdual2
+#%% Generate Matrices
+b,h = 10,10
+L = 150
+I = b*h**3/12
+A = b*h
+# Hyperdual2
+E = hyperdual2(30e3,1,0,0)
+Rho = hyperdual2(2.7e-9,0,1,0)
+K = E*I/L**3 * sparse.csc_matrix([[12,6*L,-12,6*L],[6*L,4*L**2,-6*L,2*L**2],[-12,-6*L,12,-6*L],[6*L,2*L**2,-6*L,4*L**2]])
+M = Rho*A*L/420 * sparse.csc_matrix([[156,22*L,54,-13*L],[22*L,4*L**2,13*L,-3*L**2],[54,13*L,156,-22*L],[-13*L,-3*L**2,-22*L,4*L**2]])
+
+# %% Perform eigen
+rl_val, rl_vect = hyperdual2.real_eigs(K.a,M.a,2)
+val,vect = hyperdual2.eigs(K,M,2)
+# %% Validation
+# Real
+K_0 = E.a*I/L**3 * np.matrix([[12,6*L,-12,6*L],[6*L,4*L**2,-6*L,2*L**2],[-12,-6*L,12,-6*L],[6*L,2*L**2,-6*L,4*L**2]])
+M_0 = Rho.a*A*L/420 * np.matrix([[156,22*L,54,-13*L],[22*L,4*L**2,13*L,-3*L**2],[54,13*L,156,-22*L],[-13*L,-3*L**2,-22*L,4*L**2]])
+# partial_E
+K_1 = K_0/E.a
+M_1 = np.matrix(np.zeros((4,4)))
+# partial_rho
+K_2 = np.matrix(np.zeros((4,4)))
+M_2 = M_0/Rho.a
+# Double partial
+K_12 = np.matrix(np.zeros((4,4)))
+M_12 = np.matrix(np.zeros((4,4)))
+
+max_k = np.max(np.abs([K.a-K_0,K.b-K_1,K.c-K_2,K.d-K_12]))
+argmax_k = np.argmax(np.abs([K.a-K_0,K.b-K_1,K.c-K_2,K.d-K_12]))
+max_m = np.max(np.abs([M.a-M_0,M.b-M_1,M.c-M_2,M.d-M_12]))
+argmax_m = np.argmax(np.abs([M.a-M_0,M.b-M_1,M.c-M_2,M.d-M_12]))
+
+k_arg,m_arg = np.floor((argmax_k+1)/16),np.floor((argmax_m+1)/16)
+if k_arg==0:
+    k_arg_hm = "Real"
+elif k_arg == 1:
+    k_arg_hm = "1st NonReal"
+elif k_arg == 2:
+    k_arg_hm = "2nd NonReal"
+elif k_arg == 3:
+    k_arg_hm = "3rd NonReal"
+else:
+    k_arg_hm = "error"
+if m_arg==0:
+    m_arg_hm = "Real"
+elif m_arg == 1:
+    m_arg_hm = "1st NonReal"
+elif m_arg == 2:
+    m_arg_hm = "2nd NonReal"
+elif m_arg == 3:
+    m_arg_hm = "3rd NonReal"
+else:
+    m_arg_hm = "error"
+print(f"Performing element by element comparison.")
+print(f'Max K different is {max_k} of {k_arg_hm} component.')
+print(f'Max M different is {max_m} of {m_arg_hm} component.')
+# %% Perform eigen
+# Validation
+# Real
+val_0,vect_0 = hyperdual2.real_eigs(K.a,M.a,2)
+vect_0,val_0 = np.matrix(vect_0),np.real(val_0)
+# mass normalize vectors
+a = np.diag(vect_0.T*M_0*vect_0)
+vect_0 = np.real(vect_0/np.sqrt(a))
+nm, sha = len(val_0), vect_0.shape
+val_1,val_2,val_12 = np.zeros(nm),np.zeros(nm),np.zeros(nm)
+phi_1,phi_2,phi_3 = np.matrix(np.zeros(sha)),np.matrix(np.zeros(sha)),np.matrix(np.zeros(sha))
+for i in range(nm):
+    # 1st Derivative eigenvalue
+    val_1[i] = vect_0[:,i].T * (K_1 - val_0[i] * M_1) * vect_0[:,i]
+    val_2[i] = vect_0[:,i].T * (K_2 - val_0[i] * M_2) * vect_0[:,i]
+    # 1st Derivative eigenvector
+    df1 = K_1 - val_1[i] * M_0 - val_0[i] * M_1
+    df2 = K_2 - val_2[i] * M_0 - val_0[i] * M_2
+    Finv = np.linalg.inv(K_0-val_0[i]*M_0)
+    z1 = -Finv * df1 * vect_0[:,i]
+    z2 = -Finv * df2 * vect_0[:,i]
+    c1 = float(-0.5 * vect_0[:,i].T * M_1 * vect_0[:,i] - vect_0[:,i].T * M_0 * z1)
+    c2 = float(-0.5 * vect_0[:,i].T * M_2 * vect_0[:,i] - vect_0[:,i].T * M_0 * z2)
+    phi_1[:,i] = z1 + c1 * vect_0[:,i]
+    phi_2[:,i] = z2 + c2 * vect_0[:,i]
+    # Cross Derivative eigenvalue
+    val_12[i] = vect_0[:,i].T * (K_12 - val_1[i] * M_2 - val_2[i] * M_1 - val_0[i] * M_12) * vect_0[:,i] + vect_0[:,i].T * (df1 * phi_2[:,i] + df2 * phi_1[:,i])
+
+
+max_v = np.max(np.abs([val.a-val_0,(val.b-val_1)/val.b,(val.c-val_2)/val.c,(val.d-val_12)/val.d]))
+argmax_v = np.argmax(np.abs([val.a-val_0,(val.b-val_1)/val.b,(val.c-val_2)/val.c,(val.d-val_12)/val.d]))
+
+argmax_v = np.floor((argmax_v+1)/4)
+if argmax_v==0:
+    argmax_v_hm = "Real"
+elif argmax_v == 1:
+    argmax_v_hm = "1st NonReal"
+elif argmax_v == 2:
+    argmax_v_hm = "2nd NonReal"
+elif argmax_v == 3:
+    argmax_v_hm = "3rd NonReal"
+else:
+    argmax_v_hm = "error"
+
+print(f"Performing eigenvalue comparison")
+print(f'Max Eigenvalue different is {max_v} of {argmax_v_hm} component.')
+# %%
